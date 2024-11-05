b57e0956bfc3cb19b6829a37ca2f40b6d9fc9d1c
Shengyuezzz <sz3825@nyu.edu>
2024-11-05 11:04:20 -0500

    Update PourDetector.cs

--- Cafe/Assets/Scripts/PourDetector.cs
+++ Cafe/Assets/Scripts/PourDetector.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class PourDetector : MonoBehaviour
 {
-    public float pourThreshold = 5f;  // 倾斜角度阈值
+    public float pourThreshold = 5f;  
     public Transform origin = null;
     public GameObject streamPrefab = null;
 
@@ -12,21 +12,21 @@ public class PourDetector : MonoBehaviour
 
     private void Update()
     {
-        // 获取 z 轴上的旋转角度
+        // rotation on z axis
         float zRotation = transform.eulerAngles.z;
         if (zRotation > 180)
         {
             zRotation -= 360;
         }
 
-        // 打印 z 轴旋转角度以进行调试
+        // zRotation angle
         Debug.Log("Current zRotation: " + zRotation);
 
-        // 检查旋转角度是否超过阈值
+        // if z rotation is bigger than pourthreshold
         bool pourCheck = Mathf.Abs(zRotation) > pourThreshold;
         Debug.Log("PourCheck: " + pourCheck);
 
-        // 当 pour 状态变化时，开始或结束倒液体
+        
         if (isPouring != pourCheck)
         {
             isPouring = pourCheck;
